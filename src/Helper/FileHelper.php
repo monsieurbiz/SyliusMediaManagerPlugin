144f56eedf8cfa2b0a07fc8395ad1efea14fd51e
Maxime Huran <m.huran@monsieurbiz.com>
2024-06-25 12:25:52 +0200

    Fix PHP CS

--- src/Helper/FileHelper.php
+++ src/Helper/FileHelper.php
@@ -362,11 +362,11 @@ final class FileHelper implements FileHelperInterface
 
         switch (substr($size, -1)) {
             case 't': $max *= 1024;
-            // no break
+                // no break
             case 'g': $max *= 1024;
-            // no break
+                // no break
             case 'm': $max *= 1024;
-            // no break
+                // no break
             case 'k': $max *= 1024;
         }
 
