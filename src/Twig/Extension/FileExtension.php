52f609fb76899b4bd259dcaab6b6c004f1221642
David Buros <d.buros@monsieurbiz.com>
2025-04-11 16:54:16 +0200

    feat(twig): clean extension and add new method for mime types

--- src/Twig/Extension/FileExtension.php
+++ src/Twig/Extension/FileExtension.php
@@ -13,37 +13,62 @@ declare(strict_types=1);
 
 namespace MonsieurBiz\SyliusMediaManagerPlugin\Twig\Extension;
 
-use MonsieurBiz\SyliusMediaManagerPlugin\Helper\FileHelperInterface;
-use MonsieurBiz\SyliusMediaManagerPlugin\Model\FileInterface;
+use MonsieurBiz\SyliusMediaManagerPlugin\Exception\FileNotFoundException;
+use MonsieurBiz\SyliusMediaManagerPlugin\Provider\MimeTypesProviderInterface;
+use MonsieurBiz\SyliusMediaManagerPlugin\Repository\FileRepositoryInterface;
+use MonsieurBiz\SyliusMediaManagerPlugin\Resolver\FilePathResolverInterface;
+use Symfony\Component\Filesystem\Path;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
 final class FileExtension extends AbstractExtension
 {
-    private FileHelperInterface $fileHelper;
-
-    public function __construct(FileHelperInterface $fileHelper)
-    {
-        $this->fileHelper = $fileHelper;
+    public function __construct(
+        private readonly FilePathResolverInterface $filePathResolver,
+        private readonly FileRepositoryInterface $fileRepository,
+    ) {
     }
 
     public function getFunctions(): array
     {
         return [
             new TwigFunction('get_media_manager_file_path', [$this, 'getMediaManagerFilePath']),
-            new TwigFunction('is_empty_files_list', [$this, 'isEmptyFilesList']),
+            new TwigFunction('get_mime_type', [$this, 'getMimeType']),
+            new TwigFunction('is_svg_image', [$this, 'isSvgImage']),
         ];
     }
 
     public function getMediaManagerFilePath(string $path): string
     {
-        return $this->fileHelper->getMediaPath() . '/' . $this->fileHelper->cleanPath($path);
+        return '/' . $this->filePathResolver->getMediaManagerRelativeFilePath($path);
     }
 
-    public function isEmptyFilesList(array $files): bool
+    public function getMimeType(string $relativeFilePath): ?string
     {
-        return !(bool) \count(array_filter($files, function (FileInterface $file) {
-            return !$file->isCurrentDir() && !$file->isParentDir();
-        }));
+        $absoluteFilePath = $this->filePathResolver->getAbsoluteFilePath($relativeFilePath);
+
+        try {
+            $file = $this->fileRepository->findOneFromPath($absoluteFilePath);
+
+            return $file->getMimeType();
+        } catch (FileNotFoundException) {
+            // If the file is not found in the gallery, we try to get it from the image directory
+            $absoluteFilePath = $this->filePathResolver->getAbsoluteFilePath(
+                Path::join('image', $relativeFilePath)
+            );
+
+            try {
+                $file = $this->fileRepository->findOneFromPath($absoluteFilePath);
+
+                return $file->getMimeType();
+            } catch (FileNotFoundException) {
+                return null;
+            }
+        }
+    }
+
+    public function isSvgImage(string $relativeFilePath): bool
+    {
+        return \in_array($this->getMimeType($relativeFilePath), MimeTypesProviderInterface::SVG_TYPE_MIMES, true);
     }
 }
